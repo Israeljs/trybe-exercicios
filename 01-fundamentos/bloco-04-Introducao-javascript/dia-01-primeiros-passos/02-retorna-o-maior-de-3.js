ba51b7bf0e3dbacf4a9a7a219b94c5a8ed18b90a
Israel Jeronimo <israel.nodejs@gmail.com>
2021-07-28 19:39:05 -0300

    exercicio 2

---
+++ 01-fundamentos/bloco-04-Introducao-javascript/dia-01-primeiros-passos/02-retorna-o-maior-de-3.js
@@ -0,0 +1,13 @@
+// Faça um programa que retorne o maior de três números. 
+// Defina no começo do programa três variáveis com os 
+// valores que serão comparados.
+
+let a = 5
+let b = 6
+let soma = a + b  
+
+if (a > b) {
+    console.log(a)
+} else {
+    console.log(b)
+}
