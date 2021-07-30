ad9a0b21f16a0430694bc0dd8412208e8893b68f
Israel Jeronimo <israel.nodejs@gmail.com>
2021-07-30 14:55:14 -0300

    03-Exercício media aritimética

--- 01-fundamentos/bloco-04-Introducao-javascript/dia-02-array-loop/03-media.js
+++ 01-fundamentos/bloco-04-Introducao-javascript/dia-02-array-loop/03-media.js
@@ -1,9 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
+let media = 0;
 
 for (let index = 0; index < numbers.length; index++) {
     soma += numbers[index];
-    
+    media = soma / numbers.length
 }
 
-console.log(soma)
+console.log(media)
