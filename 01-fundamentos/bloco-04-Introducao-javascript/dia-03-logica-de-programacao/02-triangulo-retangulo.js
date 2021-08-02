0862fa023c87b586d89c90eb44ee41c2282d0cd8
Israel Jeronimo <israel.nodejs@gmail.com>
2021-08-02 03:08:33 -0300

    02-Exercício imprima um triângulo retângulo com 5 asteriscos de base

---
+++ 01-fundamentos/bloco-04-Introducao-javascript/dia-03-logica-de-programacao/02-triangulo-retangulo.js
@@ -0,0 +1,14 @@
+let n = 5;
+if (n > 1) {
+    for (let index = 0; index < n; index++) {
+        let texto = "";
+        //no fim da primeira iteração o index recebe 1
+        //então o INDICE vai de 0 a 1 na primeira ieração 
+        for (let indice = 0; indice <= index; indice++) {
+            texto += "*"           
+        }
+        console.log(texto)
+    }
+} else {
+    console.log("Digite um valor maior que 1!")
+}
