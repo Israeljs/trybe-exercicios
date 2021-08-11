4885f75f3176095f38c3dda05acec2ecb25aac9d
Israel Jeronimo <israel.nodejs@gmail.com>
2021-08-11 15:41:23 -0300

    03 - Exercício eventos

---
+++ 01-fundamentos/bloco-05-javaScript-DOM-eventos-e-web-storage/dia-02-Trabalhando-com-elementos/exercicio-de-fixacao/03-removendo-elementos.js
@@ -0,0 +1,9 @@
+function removeElement(elemento) {
+    elemneto.document.removeElement(elemento)
+}
+
+
+
+
+// Remova todos os elementos filhos de paiDoPai exceto
+// pai, elementoOndeVoceEsta e primeiroFilhoDoFilho .
