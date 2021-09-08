453fd5a670aba1de704e6e68c0daef980fe8b6e9
israel jeronimo <silvaij16@gmail.com>
2021-09-07 21:42:47 -0300

    1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA

---
+++ 01-fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-03-map-filter/exercicio-fixacao/filter2.js
@@ -0,0 +1,132 @@
+const estudantes = [
+    {
+      nome: 'Jorge',
+      sobrenome: 'Silva',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 67 },
+        { name: 'Português', nota: 79 },
+        { name: 'Química', nota: 70 },
+        { name: 'Biologia', nota: 65 },
+      ],
+    },
+    {
+      nome: 'Mario',
+      sobrenome: 'Ferreira',
+      idade: 15,
+      turno: 'Tarde',
+      materias: [
+        { name: 'Matemática', nota: '59' },
+        { name: 'Português', nota: '80' },
+        { name: 'Química', nota: '78' },
+        { name: 'Biologia', nota: '92' },
+      ],
+    },
+    {
+      nome: 'Jorge',
+      sobrenome: 'Santos',
+      idade: 15,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '76' },
+        { name: 'Português', nota: '90' },
+        { name: 'Química', nota: '70' },
+        { name: 'Biologia', nota: '80' },
+      ],
+    },
+    {
+      nome: 'Maria',
+      sobrenome: 'Silveira',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '91' },
+        { name: 'Português', nota: '85' },
+        { name: 'Química', nota: '92' },
+        { name: 'Biologia', nota: '90' },
+      ],
+    },
+    {
+      nome: 'Natalia',
+      sobrenome: 'Castro',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '70' },
+        { name: 'Português', nota: '70' },
+        { name: 'Química', nota: '60' },
+        { name: 'Biologia', nota: '50' },
+      ],
+    },
+    {
+      nome: 'Wilson',
+      sobrenome: 'Martins',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '80' },
+        { name: 'Português', nota: '82' },
+        { name: 'Química', nota: '79' },
+        { name: 'Biologia', nota: '75' },
+      ],
+    },
+  ];
+
+  // Função para buscar e imprimir o nome completo de todos os estudantes que estudam no turno da manhã.
+//   const allNameStudents = [];
+
+//   for (let index = 0; index < estudantes.length; index += 1 ) {
+//     if(estudantes[index].turno === 'Manhã') {
+//       allNameStudents.push(`${estudantes[index].nome} ${estudantes[index].sobrenome}`)
+//     }
+//   }
+  
+//   console.log(allNameStudents);
+
+  // Com map e filter.
+  const allNameStudents = estudantes.filter((estudante) => (
+    estudante.turno === 'Manhã')).map((estudante) => `${estudante.nome} ${estudante.sobrenome}`);
+  
+  console.log(allNameStudents);
+
+  // Buscar um estudante pelo nome e retornar a situação dele em cada matéria:
+  // Apenas com for :
+  const findStudent = (name, students) => {
+    for (let index = 0; index < students.length; index += 1) {
+      if (students[index].nome === name) {
+        return students[index];
+      }
+    }
+  };
+  
+//   const reportStatus = (name, students) => {
+//     const getStudent = findStudent(name, students);
+//     const report = [];
+//     for (let index = 0; index < getStudent.materias.length; index += 1) {
+//       if (getStudent.materias[index].nota >= 60) {
+//         report.push(`${getStudent.materias[index].name} Aprovado`);
+//       } else {
+//         report.push(`${getStudent.materias[index].name} Reprovado`);
+//       }
+//     }
+//     return report;
+//   };
+  
+//   console.log(reportStatus('Natalia', estudantes));
+
+  // Com find e map :
+  const reportStatus = (name, students) => {
+    const studentInfo = students.find((student) => student.nome === name);
+    return studentInfo.materias.map((materia) => (
+      `${materia.name} ${(materia.nota >= 60) ? 'Aprovado' : 'Reprovado'}`
+    ));
+  };
+  
+  console.log(reportStatus('Natalia', estudantes));
+  
+//   O código teve uma redução drástica no número de linhas! Primeiro, 
+//   fizemos um find para buscar e retornar os dados do estudante. O 
+//   objeto foi retornado e salvo na variável studentsInfo , depois o 
+//   map foi usado para percorrer as matérias do objeto retornado e 
+//   salvar o que se queria em um array da forma desejada.
