087a893d0512e125d2cc1da00db2454a0234e0e0
israel jeronimo <silvaij16@gmail.com>
2021-09-10 11:25:44 -0300

    1 - Dada uma matriz, transforme em um array.

--- 01-fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-04-reduce/exercicio/exercicio1.js
+++ 01-fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-04-reduce/exercicio/exercicio1.js
@@ -8,7 +8,9 @@ const arrays = [
 ];
 
 function flatten() {
-  // escreva seu código aqui
+  return arrays.reduce((acc, curr) => acc.concat(curr), []);
 }
 
-assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
+console.log(flatten());
+
+//assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
--- 01-fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-04-reduce/exercicio/exercicio2.js
+++ 01-fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-04-reduce/exercicio/exercicio2.js
@@ -66,8 +66,4 @@ const books = [
 // 2 - Crie uma string com os nomes de todas as pessoas autoras.
 const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
 
-function reduceNames() {
-  // escreva seu código aqui
-}
-
 assert.strictEqual(reduceNames(), expectedResult);
