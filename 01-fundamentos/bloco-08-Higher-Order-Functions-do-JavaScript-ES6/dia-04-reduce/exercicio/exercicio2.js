8e8d740c17622f4ae3650f0e0edce4fb5059c0f9
israel jeronimo <silvaij16@gmail.com>
2021-09-10 11:26:44 -0300

    2 - Crie uma string com os nomes de todas as pessoas autoras.

--- 01-fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-04-reduce/exercicio/exercicio2.js
+++ 01-fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-04-reduce/exercicio/exercicio2.js
@@ -66,4 +66,12 @@ const books = [
 // 2 - Crie uma string com os nomes de todas as pessoas autoras.
 const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
 
+function reduceNames() {
+  const names = books.reduce((acc, book, index, array) => {
+    if (index === array.length - 1) return `${acc} ${book.author.name}.`;
+    return `${acc} ${book.author.name},`;
+  }, '');
+  return names.trim();
+}
+console.log(reduceNames());
 assert.strictEqual(reduceNames(), expectedResult);
