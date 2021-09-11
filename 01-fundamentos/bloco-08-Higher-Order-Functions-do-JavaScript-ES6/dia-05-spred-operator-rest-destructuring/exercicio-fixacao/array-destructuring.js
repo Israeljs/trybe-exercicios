cd912a3a7d56eb6d2f246d5a1012eaf2a653bd34
israel jeronimo <silvaij16@gmail.com>
2021-09-11 20:41:34 -0300

    exercicio de fixação array destructuring

---
+++ 01-fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-05-spred-operator-rest-destructuring/exercicio-fixacao/array-destructuring.js
@@ -0,0 +1,50 @@
+const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
+const [firstCountry, secondCountry, thirdCountry, fourthCountry] = arrayCountries;
+
+console.log(firstCountry); // Brazil
+console.log(secondCountry); // Japan
+console.log(thirdCountry); // China
+console.log(fourthCountry); // Canada
+
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+//saudacoes[1](saudacoes[0]); // Olá
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+const [saudacao, realizaSaudacao] = saudacoes;
+
+realizaSaudacao(saudacao) // Olá
+
+// 2 - A seguir, temos alguns valores que estão descritos em variáveis incoerentes.
+// Através da desestruturação de arrays, corrija os valores das variáveis.
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam 
+// nas variáveis correspondentes ao seu verdadeiro tipo
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+[comida, animal, bebida] = [bebida, comida, animal]; // Criando o array e o desestruturando
+console.log(comida, animal, bebida); // arroz gato água
+
+// 3 - array abaixo possui alguns números que não condizem com
+//  o conteúdo que ele deveria possuir. Através de array destructuring, 
+//  faça com que existam apenas números pares na variável numerosPares.
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+// console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+[,,, ...numerosPares] = numerosPares;
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
