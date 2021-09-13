ac9e3e43f381af74b03bbab30c13d4d1a35fb0aa
israel jeronimo <silvaij16@gmail.com>
2021-09-13 12:30:02 -0300

    exercício/8.5 refatoração

--- 01-fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-05-spred-operator-rest-destructuring/exercicio/exercicio-04.js
+++ 01-fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-05-spred-operator-rest-destructuring/exercicio/exercicio-04.js
@@ -33,15 +33,13 @@ const people = [
 ];
 
 // escreva filterPeople abaixo
+const filterPeople = (arr) => arr.filter(
+  ({ nationality, bornIn }) =>
+    nationality === 'Australian'
+    && bornIn > 1900 && bornIn <= 2000,
+  );
 
-const filteredPeople = (people) => {
-  const { name, bornIn, nationality } = people;
-  return {
-    name,
-    bornIn,
-    nationality
-  }
-};
+const filteredPeople = filterPeople(people);
 
 assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
 assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });
