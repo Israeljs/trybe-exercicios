bf79b351000474fd7f4659da09a7ee84402b9987
israel jeronimo <silvaij16@gmail.com>
2021-09-15 15:58:17 -0300

    Exercício de fixação callbacks

--- 01-fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-04-reduce/aula/reduce3.js
+++ 01-fundamentos/bloco-08-Higher-Order-Functions-do-JavaScript-ES6/dia-04-reduce/aula/reduce3.js
@@ -24,3 +24,23 @@ const playersObject = players.reduce((accumulator, player) => {
 console.log(playersObject)
 
 // assert.deepStrictEqual(playersObject, expected);
+
+// function countEntrants(entrants) {
+//   const child = entrants.filter(({ age }) => age < 18).length;
+//   const adult = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+//   const senior = entrants.filter(({ age }) => age >= 50).length;
+//   const obj = { child, adult, senior };
+//   return obj;
+// }
+// console.log(countEntrants(entrants));
+
+// const { prices } = require('../data/zoo_data');
+
+// const entrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
