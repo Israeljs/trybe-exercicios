af3967ba94d934336989c216f9a3f41f16c5baa8
israel jeronimo <silvaij16@gmail.com>
2021-09-19 12:19:00 -0300

    exercicio 4

--- 01-fundamentos/bloco-09-JavaScript-Assíncrono-e-Promises/dia-02-javascript-Promises/exercicio/exercicio04.js
+++ 01-fundamentos/bloco-09-JavaScript-Assíncrono-e-Promises/dia-02-javascript-Promises/exercicio/exercicio04.js
@@ -1,1 +1,21 @@
-
+// Quando a Promise for rejeitada, imprima, via console.log , a frase "É mais de oito mil! Essa promise deve estar quebrada!"
+const fetchPromise = () => {
+    const myPromise = new Promise((resolve, reject) => {
+      const myArray = Array.from(
+        { length: 10 },
+        () =>  Math.floor(Math.random() * 50) + 1
+      );
+      const sum = myArray.map(number => number * number)
+                         .reduce((number, acc) => number + acc, 0);
+  
+      (sum < 8000) ? resolve(sum) : reject();
+    });
+  
+    myPromise
+      .then(sum => [2, 3, 5, 10].map(number => sum / number))
+      .catch(() =>
+        console.log('É mais de oito mil! Essa promise deve estar quebrada!')
+      );
+  };
+  
+  fetchPromise();
