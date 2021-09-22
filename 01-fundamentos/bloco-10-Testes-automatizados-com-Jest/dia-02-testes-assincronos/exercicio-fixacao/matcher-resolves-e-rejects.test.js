af7e9ecd0ea544e0fdf53d6714c6ff5fcffa8d7a
israel jeronimo <silvaij16@gmail.com>
2021-09-22 19:58:00 -0300

    exercicio de fixação matcher resolves rejects refatorado

--- 01-fundamentos/bloco-10-Testes-automatizados-com-Jest/dia-02-testes-assincronos/exercicio-fixacao/matcher-resolves-e-rejects.test.js
+++ 01-fundamentos/bloco-10-Testes-automatizados-com-Jest/dia-02-testes-assincronos/exercicio-fixacao/matcher-resolves-e-rejects.test.js
@@ -17,3 +17,16 @@ describe('Testando promise - findAnimalsByType', () => {
     });
   });
 
+  describe('Jest', () => {
+    test('Brincando com expect', () => {
+      const expectA = expect(true).toBeTruthy();
+      const expectB = expect(Promise.resolve('Oi Tryber!')).resolves.toEqual('Oi Tryber!');
+      const expectC = expect(Promise.reject(new Error('ERROR')))
+        .rejects.toEqual(new Error('ERROR'));
+      console.log('expect', expectA);
+      console.log('expect.resolves', expectB);
+      console.log('expect.rejects', expectC);
+    });
+  });
+  // Perceba que o retorno do primeiro expect (que não tem os matchers) é undefined, 
+  // porém o retorno do segundo e do terceiro (que tem os matchers) é uma promise .
