bc44661a8f529f35cdc60627e66da4729794a58f
israel jeronimo <silvaij16@gmail.com>
2021-11-29 18:50:51 -0300

    converter uma temperatura medida em um termômetro em Fahrenheits  para a escala Celsius

--- 01-fundamentos/logica/01-conversor-fahrenheit-celsius.js
+++ 01-fundamentos/logica/01-conversor-fahrenheit-celsius.js
@@ -22,8 +22,8 @@ const { strict: assert } = require('assert');
 */
 
 function fahrenheit_to_celsius(temp_fahrenheit) {
+  return ((temp_fahrenheit - 32) / 1.8).toFixed(1);
 };
-// console.log(fahrenheit_to_celsius(5))
 
 // @TESTES
 function test_hackerrank() {
