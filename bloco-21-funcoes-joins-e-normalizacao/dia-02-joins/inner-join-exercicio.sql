d6daeb1bf31e99f0c5613cd4e4bb8b3f0a344d29
israel jeronimo <silvaij16@gmail.com>
2022-02-01 19:18:31 -0300

    2 - Use o JOIN para exibir o nome , sobrenome e endereço de cada um dos funcionários do banco. Use as tabelas staff e address

--- bloco-21-funcoes-joins-e-normalizacao/dia-02-joins/inner-join-exercicio.sql
+++ bloco-21-funcoes-joins-e-normalizacao/dia-02-joins/inner-join-exercicio.sql
@@ -5,3 +5,11 @@ AS 'Nome do autor', f.film_id
 FROM actor AS act
 INNER JOIN film_actor f
 ON act.actor_id = f.actor_id;
+
+-- Use o JOIN para exibir o nome , sobrenome e endereço de cada um dos 
+-- funcionários do banco. Use as tabelas staff e address .
+SELECT st.first_name AS Nome, st.last_name AS 'Sobre nome',
+ad.address AS Endereco
+FROM staff AS st
+INNER JOIN address AS ad
+ON st.address_id = ad.address_id;
