fee61653fc1fdbaf6d22e4e90e178478af4eaf6f
israel jeronimo <silvaij16@gmail.com>
2022-02-02 22:25:31 -0300

    2: Utilizando o INNER JOIN , faça uma busca que retorne o número de vendas para cada filme que possui um número  maior de vendas internacionais ( international_sales ) do que vendas nacionais ( domestic_sales ).

--- bloco-21-funcoes-joins-e-normalizacao/dia-02-joins/exercicio.sql
+++ bloco-21-funcoes-joins-e-normalizacao/dia-02-joins/exercicio.sql
@@ -5,3 +5,10 @@ FROM Pixar.Movies AS m
 INNER JOIN Pixar.BoxOffice AS b
 ON m.id = b.movie_id;
 
+-- 2: Utilizando o INNER JOIN , faça uma busca que retorne o número de vendas para cada filme que possui um número
+--  maior de vendas internacionais ( international_sales ) do que vendas nacionais ( domestic_sales ).
+SELECT m.title, b.international_sales AS 'número de vendas'
+FROM Pixar.Movies AS m
+INNER JOIN Pixar.BoxOffice AS b
+ON m.id = b.movie_id
+WHERE b.international_sales > b.domestic_sales;
