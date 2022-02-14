034c74be4e2b63e2e60cc822477d7e0ad9746cf5
israel jeronimo <silvaij16@gmail.com>
2022-02-13 23:20:51 -0300

    mentoria de lógica desafio 3

---
+++ 01-fundamentos/logica/logica-mentoria/desafio03.js
@@ -0,0 +1,13 @@
+// Dada uma string com uma frase, retorne uma nova frase invertendo todas as
+// palavras que tenham cinco ou mais letras.
+// Não altere a string original.
+/* 
+  'Trybe'; => ebyrT
+  'Olá queridos amigos'; => Olá sodireuq sogima
+  'Isso é um gato'; => Isso é um gato
+  'Você está quase no último teste'; => Você está esauq no omitlú etset
+  'A base do teto desaba'; => A base do teto abased
+ */
+const spinWords = (string) => {};
+
+module.exports = spinWords
