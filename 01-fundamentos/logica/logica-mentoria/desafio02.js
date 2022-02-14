fa89b27c68e327b6bd943fddc174188d1d981bed
israel jeronimo <silvaij16@gmail.com>
2022-02-13 23:20:24 -0300

    mentoria de lógica desafio 2

---
+++ 01-fundamentos/logica/logica-mentoria/desafio02.js
@@ -0,0 +1,12 @@
+// Dado um array de inteiros, retorna a soma dos valores únicos (que não se repentem) do array.
+// Caso o array for vazio, retornar null
+
+const firstArr = [1, 2, 3]; // 6
+const secondArr = [8, 1, 1, 3, 1, 8]; // 3
+const thirdArr = [-1, -1, 5, 2, -7]; // 0
+const nullArr = []; // null
+
+const uniqueSum = (arr) => {};
+
+
+module.exports = uniqueSum
