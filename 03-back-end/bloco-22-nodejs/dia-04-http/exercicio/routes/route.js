f7efa20e4c2f441ba929f512b0dd281561e3785d
israel jeronimo <silvaij16@gmail.com>
2022-02-17 18:36:20 -0300

    exercicio-23-01

--- 03-back-end/bloco-22-nodejs/dia-04-http/exercicio/routes/route.js
+++ 03-back-end/bloco-22-nodejs/dia-04-http/exercicio/routes/route.js
@@ -4,22 +4,22 @@ const bodyParser = require('body-parser');
 const router = Router();
 
 router
-  .get('/ping', handlePong)
-  .post('/hello', handleHello)
-  .put('/users/:name/:age', handleNameAge);
+  .get('/ping', handlePong);
+  // .post('/hello', handleHello)
+  // .put('/users/:name/:age', handleNameAge);
 
 function handlePong(req, res) {
   return res.status(200).json({ "message": 'pong' });
 }
 
-function handleHello (req, res) {
-  const { name } = req.body;
-  return res.status(200).json({ "message": `Hello, ${name}!` })
-}
+// function handleHello (req, res) {
+//   const { name } = req.body;
+//   return res.status(200).json({ "message": `Hello, ${name}!` })
+// }
 
-function handleNameAge(req, res) {
-  const { name, age } = req.params;
-  return res.status(200).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` })
-}
+// function handleNameAge(req, res) {
+//   const { name, age } = req.params;
+//   return res.status(200).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` })
+// }
 
 module.exports = router;
