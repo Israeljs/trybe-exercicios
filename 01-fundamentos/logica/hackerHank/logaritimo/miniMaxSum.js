074279f1cf689ddcf2eb97984cfa40a8e3a8f356
israel jeronimo <silvaij16@gmail.com>
2022-05-11 09:43:45 -0300

    birthdayCakeCandles

--- 01-fundamentos/logica/hackerHank/logaritimo/miniMaxSum.js
+++ 01-fundamentos/logica/hackerHank/logaritimo/miniMaxSum.js
@@ -1,25 +1,35 @@
+// function miniMaxSum(aarr) {
+//   let arr = aarr.split(' ')
+//   let sum1 = 0;
+//   let sum2 = 0;
+//   let result = '';
+
+//   for (let i = 1; i < arr.length; i++) {
+//     sum1 += parseInt(arr[i]);
+//   }
+
+//   for (let j = 0; j < arr.length - 1; j++) {
+//     sum2 += parseInt(arr[j]);
+//   }
+
+//   if (sum1 > sum2) {
+//     return console.log(`${sum2} ${sum1}`);
+//   }else {
+//     return console.log(`${sum2} ${sum1}`);
+//   }
+
+// }
+
+
 function miniMaxSum(arr) {
-  let sum = 0;
-  let ar = [];
-  let result = '';
+    let sum = arr.reduce((a, b) => {
+        return a + b;
+    });
+    const min = sum - Math.max(...arr);
+    const max = sum - Math.min(...arr);
 
-  for (let i = 0; i < arr.length; i++) {
-    sum = 0;
-    for (let j = 0; j < arr.length; j++) {
-      if (i !== j) {
-        sum += arr[j];
-      }
-    }
-    ar.push(sum);
-  }
-
-  ar.sort();
-
-  for (let x = 0; x < ar.length; x++) {
-    result += `${ar[x]} `;
-    
-  }
-return console.log(result);
+    console.log(`${min} ${max}`);
 }
 
 miniMaxSum([1, 3, 5, 7, 9]);
+// miniMaxSum(13579);
