87a8adee1467424ed039b01aa35dc14a58f3d1a8
israel jeronimo <silvaij16@gmail.com>
2022-10-18 12:32:18 -0300

    feat: python quociente

--- 04-ciencia-da-computacao/bloco-01-python/dia-01-introducao/aula.py
+++ 04-ciencia-da-computacao/bloco-01-python/dia-01-introducao/aula.py
@@ -5,3 +5,15 @@ names = ['israel', 'bebel', 'natalia']
 square_root = 25 ** 2 #(1/2)  # raiz quadrada de 25. O operador `**` significa "elevado a"
 
 print(square_root + 1)  # saída: 6.0
+
+
+counter = 0
+# original
+counter = counter + 1
+
+# simplificado
+counter += 1
+
+# 3 // 2  # saída: 1 quociente.
+
+# 3 / 2  # saída: 1.5
