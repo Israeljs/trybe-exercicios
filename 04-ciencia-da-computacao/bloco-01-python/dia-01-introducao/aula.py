dd4e24bd03bffafa3995fed2b3a672b73df4fc8f
israel jeronimo <silvaij16@gmail.com>
2022-10-20 16:38:07 -0300

    feat: exercicio-01 5

--- 04-ciencia-da-computacao/bloco-01-python/dia-01-introducao/aula.py
+++ 04-ciencia-da-computacao/bloco-01-python/dia-01-introducao/aula.py
@@ -38,6 +38,17 @@ print(type(a))
 # sequências binárias(bytes, bytearray, memoryview).
 
 # listas - arrays
+# uma lista é uma sequência mutável e ordenada de elementos. Ela pode armazenar elementos
+#  heterogêneos, ter seu tamanho variável e crescer à medida que itens são adicionados.
+fruits = ["laranja", "maçã", "uva", "abacaxi"]  # elementos são definidos separados por vírgula, envolvidos por colchetes
+fruits[0]  # o acesso é feito por índices iniciados em 0
+fruits[-1]  # o acesso também pode ser negativo
+fruits.append("banana")  # adicionando uma nova fruta
+fruits.remove("abacaxi")  # removendo uma fruta
+fruits.extend(["pera", "melão", "kiwi"])  # acrescenta uma lista de frutas a lista original
+fruits.index("maçã")  # retorna o índice onde a fruta está localizada, neste caso, 1
+fruits.sort()  # ordena a lista de frutas
+
 
 chess_player = ['Magnus', 'Carlos', 'Fabiano']
 print(chess_player) # ['Magnus', 'Carlos', 'Fabiano']
--- 04-ciencia-da-computacao/bloco-01-python/dia-01-introducao/exercicio-01.py
+++ 04-ciencia-da-computacao/bloco-01-python/dia-01-introducao/exercicio-01.py
@@ -4,14 +4,18 @@ result1 = a % b # módulo
 result2 = a // b # divisão inteira
 print(result1, result2)
 
-
+# 3
 hours = 6
 minutes = 60 * hours
 seconds = minutes * 60
 print(minutes, seconds)
-
+#4
 books = 60
 book_price = (1 - 0.4) * 24.20
 logistic = 3 + (books - 1) * 0.75
 cost = books * book_price + logistic
 print(format(cost, ".2f"))
+
+trybe_course = ["Introdução", "Front-end", "Back-end"]
+#5
+trybe_course.append("Ciência da Computação")
