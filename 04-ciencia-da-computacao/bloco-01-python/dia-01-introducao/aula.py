18361180ff75f801ff46bed39b60a7050ce66ad5
israel jeronimo <silvaij16@gmail.com>
2022-10-21 00:08:10 -0300

    feat: aula-01 range

--- 04-ciencia-da-computacao/bloco-01-python/dia-01-introducao/aula.py
+++ 04-ciencia-da-computacao/bloco-01-python/dia-01-introducao/aula.py
@@ -115,6 +115,23 @@ standard_user = all_user.difference(user)
 # Acesso à todos
 user = user.union(standard_user)
 
+# Range
+# Estrutura capaz de gerar uma sequência numérica de um valor inicial até um 
+# vamos converter o range em uma lista para ajudar na visualização
+
+# definimos somente o valor de parada
+list(range(5))  # saída: [0, 1, 2, 3, 4]
+
+# definimos o valor inicial e o de parada
+list(range(1, 6))  # saída: [1, 2, 3, 4, 5]
+
+# definimos valor inicial, de parada e modificamos o passo para 2
+list(range(1, 11, 2))  # saída: [1, 3, 5, 7, 9]
+
+# podemos utilizar valores negativos para as entradas também
+list(range(10, 0, -1))  # saída: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+
 # lista - se armazena entidades relacionadas. Por exemplo os diferentes elementos de uma lista de supermercados pão, manteiga e leite
 # tupla - se armazena informações de uma mesma entidade por exemplo a manteiga é um laticínio, porcentagem de gordura se tem sal
 # dicionário - usamos quando se quer associar um valor a uma chave específica. Tanto pode ser usado no lugar de uma lista quanto de uma tupla.
