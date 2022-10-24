f5acb7aa45863c23e88664269f3c81a0e3b52222
israel jeronimo <silvaij16@gmail.com>
2022-10-24 12:07:35 -0300

    feat: python aula-01 - função

--- 04-ciencia-da-computacao/bloco-01-python/dia-01-introducao/aula.py
+++ 04-ciencia-da-computacao/bloco-01-python/dia-01-introducao/aula.py
@@ -233,3 +233,33 @@ for index, language in enumerate(['Python', 'Java']):
 # Saída:
 # 0 - Python
 # 1 - Java
+
+# função
+def concat(*strings):
+    # Equivalente a um ", ".join(strings), que concatena os elementos de um iterável em uma string utilizando um separador
+    # Nesse caso a string resultante estaria separada por vírgula
+    final_string = ""
+    for string in strings:
+        final_string += string
+        if not string == strings[-1]:
+            final_string += ', '
+    return final_string
+
+# pode ser chamado com 2 parâmetros
+print(concat("Carlos", "Cristina"))  # saída: "Carlos, Cristina"
+
+# pode ser chamado com um número n de parâmetros
+print(concat("Carlos", "Cristina", "Maria"))  # saída: "Carlos, Cristina, Maria"
+
+# dict é uma função que já vem embutida no python
+print(dict(nome="Felipe", sobrenome="Silva", idade=25))  # cria um dicionário utilizando as chaves passadas
+
+print(dict(nome="Ana", sobrenome="Souza", idade=21, turma=1))  # o número de parâmetros passados para a função pode variar
+
+len([1, 2, 3, 4])  # função len não aceita argumentos nomeados
+
+# len(obj=[1, 2, 3, 4])  # este código irá falhar
+
+print("Coin", "Rodrigo", ", ")  # imprime Coin Rodrigo ,
+
+print("Coin", "Rodrigo", sep=", ")  # nomeando o terceiro parâmetro, agora temos a saída: Coin, Rodrigo
